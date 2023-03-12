972c5a531126d2d2eb8255a59fde1556e925a8a8
Soumajyoti <soumyabwn3@gmail.com>
2023-03-12 22:25:45 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const NewsItem = (props) => {
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+
+    // let [title, description, imageUrl, newsUrl, author, date, source] = props;  ---> Wrong Way of destructuring
+
+    return (
+        <div className='my-3'>
+            <div className="card">
+
+                <div className='' style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    right: '0',
+                    position: 'absolute',
+                }}>
+                    <span className="badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+                </div>
+
+                <img src={imageUrl ? imageUrl : "https://images.unsplash.com/photo-1614028674490-d12fd4eaa168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&height=600&q=80"} className="card-img-top" alt="newsImage" />
+                <div className="card-body">
+                    <h5 className="card-title">{title} </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default NewsItem
